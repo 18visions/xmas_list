a3a81af8d91b6871ee5006b62b159e75d4064bec
18visions <nick.turner82@gmail.com>
2020-04-26 16:04:54 -0700

    Added try/catch block

--- create_users_table.py
+++ create_users_table.py
@@ -1,18 +1,16 @@
 from users_table import Base
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
-from os import environ
+from sqlalchemy import create_engine
 from config import credential
 import pyodbc
-import urllib.parse
 
-
-engine = create_engine('mssql+pyodbc://{}:{}@{}:{}/{}}?driver=SQL+Server'
-                       .format(credential.dbcreds['username'],
-                               credential.dbcreds['password'],
-                               credential.dbcreds['host'],
-                               credential.dbcreds['port'],
-                               credential.dbcreds['dbname'])
-                       )
-
-Base.metadata.create_all(engine, checkfirst=True)
+try:
+    engine = create_engine('mssql+pyodbc://{}:{}@{}:{}/{}?driver=SQL+Server'
+                           .format(credential.dbcreds['username'],
+                                   credential.dbcreds['password'],
+                                   credential.dbcreds['host'],
+                                   credential.dbcreds['port'],
+                                   credential.dbcreds['dbname'])
+                           )
+    Base.metadata.create_all(engine, checkfirst=True)
+except Exception as e:
+    print(e)
