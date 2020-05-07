c8011b931f47406eacce454378c9e594b91fd9db
18visions <nick.turner82@gmail.com>
2020-05-07 09:17:24 -0700

    added functionality to reminder_text that checks for the last sms

--- reminder_text.py
+++ reminder_text.py
@@ -1,17 +1,28 @@
 import config
 from twilio.rest import Client
-from user_actions import get_all_user_items, get_user_phone
+from user_actions import get_all_user_items, get_user_phone, last_sent_sms
+from datetime import datetime, timedelta
 
 
 def user_reminder(userid):
     useritems = get_all_user_items(userid)
-    print(useritems)
+    lastsms = last_sent_sms(userid)
+    twentyfourhoursago = datetime.now() - timedelta(hours=24)
     account_sid = config.credential.twiliocreds['acct_sid']
     auth_token = config.credential.twiliocreds['auth_token']
     fromNumber = config.credential.twiliocreds['from_number']
     if useritems:
         pass
-    else:
+    elif not useritems:
+        if lastsms < twentyfourhoursago:
+            userPhone = get_user_phone(userid)
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                to=userPhone,
+                from_=fromNumber,
+                body="Just a friendly reminder to add items to your Turner family wish list :)")
+            print(message.sid)
+    elif lastsms < twentyfourhoursago:
         userPhone = get_user_phone(userid)
         client = Client(account_sid, auth_token)
         message = client.messages.create(
@@ -22,5 +33,4 @@ def user_reminder(userid):
 
 
 if __name__ == '__main__':
-    user_reminder(1)
-
+    user_reminder(1)  # testing/debugging
--- user_actions.py
+++ user_actions.py
@@ -1,5 +1,5 @@
-from table_definitions import User, userItems
-from sqlalchemy import create_engine, exists
+from table_definitions import User, userItems, smsmessages
+from sqlalchemy import create_engine, exists, desc
 from sqlalchemy.orm import sessionmaker
 from config import credential
 from datetime import datetime
@@ -19,14 +19,15 @@ session = Session()
 def create_new_user(firstname, lastname, email, phonenumber):
     createdate_now = datetime.now()
     try:
-        doesUserExist = session.query(User).filter(User.firstname == firstname).all()
+        doesUserExist = session.query(User)\
+            .filter(User.firstname == firstname)\
+            .all()
         if not doesUserExist:
             newUser = User(firstname=firstname,
                            lastname=lastname,
                            email=email,
                            phonenumber=phonenumber,
-                           dateAdded=createdate_now
-                            )
+                           dateAdded=createdate_now)
             session.add(newUser)
             session.commit()
             session.close()
@@ -51,7 +52,9 @@ def create_new_user_item(userId, description, shoppingLink):
 
 def get_user_item(itemId):
     try:
-        getItem = session.query(userItems.itemId).filter(userItems.itemId == itemId).all()
+        getItem = session.query(userItems.itemId)\
+            .filter(userItems.itemId == itemId)\
+            .all()
         for x in getItem:
             print(x[0])
             return x[0]
@@ -61,9 +64,10 @@ def get_user_item(itemId):
 
 def get_all_user_items(userid):
     try:
-        getAll = session.query(userItems).filter(userItems.userId == userid).all()
+        getAll = session.query(userItems)\
+            .filter(userItems.userId == userid)\
+            .all()
         for x in getAll:
-            print(x)
             return x
     except Exception as e:
         print(e)
@@ -71,7 +75,9 @@ def get_all_user_items(userid):
 
 def get_user_phone(userid):
     try:
-        getPhone = session.query(User.phonenumber).filter(User.userId == userid).all()
+        getPhone = session.query(User.phonenumber)\
+            .filter(User.userId == userid)\
+            .all()
         for phone in getPhone:
             return phone[0]
     except Exception as e:
@@ -82,7 +88,9 @@ def delete_user_item(uid):
     itemId = uid
     itemToDelete = get_user_item(itemId)
     try:
-        getItem = session.query(userItems).filter(userItems.itemId == itemToDelete).delete()
+        getItem = session.query(userItems)\
+            .filter(userItems.itemId == itemToDelete)\
+            .delete()
         session.commit()
         session.close()
         print(getItem)
@@ -90,10 +98,37 @@ def delete_user_item(uid):
         print(e)
 
 
+def sms_log(userid,tonumber,twiliosid):
+    createdate_now = datetime.now()
+    try:
+        log_sms = smsmessages(userId=userid,
+                              toNumber=tonumber,
+                              twilioSid=twiliosid,
+                              dateSent=createdate_now)
+        session.add(log_sms)
+        session.commit()
+        session.close()
+    except Exception as e:
+        print(e)
+
+
+def last_sent_sms(userid):
+    try:
+        get_last = session.query(smsmessages)\
+            .filter(smsmessages.userId == userid)\
+            .order_by(smsmessages.dateSent.desc())\
+            .limit(1)
+        for x in get_last:
+            return x.dateSent
+    except Exception as e:
+        print(e)
+
 if __name__ == "__main__":
-    create_new_user_item(1, 'itemstuff', 'www.google.com')
-    #create_new_user('nick', 'turner', 'test@test.com', '3609493180')
-    #get_user_item(8)
-    #delete_user_item(5)
-    #get_all_user_items(1)
-    #get_user_phone(1)
+    #create_new_user_item(1, 'itemstuff', 'www.google.com')  # testing/debugging
+    create_new_user('nick', 'turner', 'test@test.com', '3609493180')  # testing/debugging
+    #get_user_item(8)  # testing/debugging
+    #delete_user_item(5)  # testing/debugging
+    #get_all_user_items(1)  # testing/debugging
+    #get_user_phone(1)  # testing/debugging
+    #sms_log(1, '3609493180', 'abc1236')
+    #last_sent_sms(1)
